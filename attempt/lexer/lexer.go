185d12f1b7b5d54303776f2f21a8417c6ba6c144
Marcus <marcushoks@gmail.com>
2025-06-10 00:54:14 +0800

    sync: page 25

--- attempt/lexer/lexer.go
+++ attempt/lexer/lexer.go
@@ -30,11 +30,37 @@ func (l *Lexer) readChar() {
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
+	l.skipWhitespace()
+
 	switch l.char {
 	case '=':
-		tok = token.New(token.ASSIGN, l.char)
+		if l.peekChar() == '=' {
+			char := l.char
+			l.readChar()
+			tok = token.Token{Type: token.EQ, Literal: string(char) + string(l.char)}
+		} else {
+			tok = token.New(token.ASSIGN, l.char)
+		}
 	case '+':
 		tok = token.New(token.PLUS, l.char)
+	case '-':
+		tok = token.New(token.MINUS, l.char)
+	case '*':
+		tok = token.New(token.ASTERISK, l.char)
+	case '/':
+		tok = token.New(token.SLASH, l.char)
+	case '<':
+		tok = token.New(token.LT, l.char)
+	case '>':
+		tok = token.New(token.GT, l.char)
+	case '!':
+		if l.peekChar() == '=' {
+			char := l.char
+			l.readChar()
+			tok = token.Token{Type: token.NOT_EQ, Literal: string(char) + string(l.char)}
+		} else {
+			tok = token.New(token.BANG, l.char)
+		}
 	case '(':
 		tok = token.New(token.LPAREN, l.char)
 	case ')':
@@ -53,6 +79,12 @@ func (l *Lexer) NextToken() token.Token {
 	default:
 		if isLetter(l.char) {
 			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		}
+		if isDigit(l.char) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
 			return tok
 		}
 		tok = token.New(token.ILLEGAL, l.char)
@@ -60,6 +92,19 @@ func (l *Lexer) NextToken() token.Token {
 
 	l.readChar()
 	return tok
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.char == ' ' || l.char == '\t' || l.char == '\n' || l.char == '\r' {
+		l.readChar()
+	}
+}
+
+func (l *Lexer) peekChar() rune {
+	if l.readPosition >= len(l.input) {
+		return 0
+	}
+	return l.input[l.readPosition]
 }
 
 func (l *Lexer) readIdentifier() string {
@@ -70,8 +115,20 @@ func (l *Lexer) readIdentifier() string {
 	return string(l.input[position:l.position])
 }
 
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.char) {
+		l.readChar()
+	}
+	return string(l.input[position:l.position])
+}
+
 func isLetter(char rune) bool {
 	return 'a' <= char && char <= 'z' ||
 		'A' <= char && char <= 'Z' ||
 		char == '_'
+}
+
+func isDigit(char rune) bool {
+	return '0' <= char && char <= '9'
 }
--- attempt/token/token.go
+++ attempt/token/token.go
@@ -9,8 +9,17 @@ const (
 	INT   = "INT"
 
 	// operators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT     = "<"
+	GT     = ">"
+	EQ     = "=="
+	NOT_EQ = "!="
 
 	// delimiters
 	COMMA     = ","
@@ -24,11 +33,21 @@ const (
 	// keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
