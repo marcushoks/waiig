194087ed228154497aea7b632a18ab34b14b38ed
Marcus <marcushks@gmail.com>
2025-08-10 20:43:02 +0800

    sync: page 40

--- attempt/parser/parser.go
+++ attempt/parser/parser.go
@@ -29,5 +29,31 @@ func (p *Parser) nextToken() {
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	}
 	return nil
+}
+
+func (p *Parser) parseLetStatement() *ast.Statement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
 }
